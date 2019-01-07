8a044f21a27cc2132cc176f9bb3a6bed69241e34
John Christman <jchristman@exida.com>
2019-01-06 20:04:22 -0500

    added Guid to entities

--- myDash.Shared/Settings/WidgetSettingsBase.cs
+++ myDash.Shared/Settings/WidgetSettingsBase.cs
@@ -1,7 +1,10 @@
-﻿namespace myDash.Shared
+﻿using System;
+
+namespace myDash.Shared
 {
     public class WidgetSettingsBase
     {
+        public Guid Guid { get; set; }
         public virtual string widget { get; set; }
         public virtual int colsLarge { get; set; } = 4;
         public virtual int colsSmall { get; set; } = 6;
