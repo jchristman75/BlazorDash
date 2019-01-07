52313239cf3ff159b8441ab6fd834efcd56e90f2
John Christman <jchristman@exida.com>
2019-01-06 19:54:45 -0500

    derived settings classes.

--- myDash.Server/Controllers/SampleDataController.cs
+++ myDash.Server/Controllers/SampleDataController.cs
@@ -35,21 +35,18 @@ namespace myDash.Server.Controllers
                 new LifeCountdownSetting(){Header = "Jim", BirthDate = new DateTime(1970,1,1), EndAge = 70},
                 new LifeCountdownSetting(){Header = "Bob", BirthDate = new DateTime(1990,7,4), EndAge = 75},
                 new LifeCountdownSetting(){Header = "Rick", BirthDate = new DateTime(1985,4,27), EndAge = 80},
-                new WidgetSettings() {widget = "SimpleCard", parameters = {{"Header", "This is a simple card"}, {"SubHeader", ""},
-                    { "Body","This is the body of my simple card.  There are other simple cards, but this is mine."},
-                    { "colsLarge", 6}, {"colsSmall", 12}}},
-                new WidgetSettings() {widget = "Countdown", parameters =
-                {
-                    {"Header","Countdown to New Year" },
-                    {"From",$"{DateTime.Today.Year}/1/1"},
-                    {"To", $"{DateTime.Today.Year+1}/1/1"}
-                }},
-                new WidgetSettings() {widget = "Countdown", parameters =
-                {
-                    {"Header","Countdown to July 4th" },
-                    {"From",$"{DateTime.Today.Year}/7/4"},
-                    {"To", $"{DateTime.Today.Year+1}/7/4"}
-                }},
+                new SimpleCardSetting() {Header= "This is a simple card",
+                    SubHeader = "",
+                    Body ="This is the body of my simple card.  There are other simple cards, but this is mine.",
+                    colsLarge= 6, colsSmall= 12},
+                new CountdownSetting() {Header = "Countdown to New Year",
+                    StartTime = new DateTime(DateTime.Today.Year,1,1),
+                    EndTime = new DateTime(DateTime.Today.Year+1,1,1)
+                },
+                new CountdownSetting() {Header = "Countdown to July 4th",
+                    StartTime = new DateTime(DateTime.Today.Year,7,4),
+                    EndTime = new DateTime(DateTime.Today.Year+1,7,4)
+                },
                 new WidgetSettings() {widget = "CanvasWidget"}
             };
             return widgets;
--- myDash.Shared/Settings/WidgetSettingsBase.cs
+++ myDash.Shared/Settings/WidgetSettingsBase.cs
@@ -3,5 +3,7 @@
     public class WidgetSettingsBase
     {
         public virtual string widget { get; set; }
+        public virtual int colsLarge { get; set; } = 4;
+        public virtual int colsSmall { get; set; } = 6;
     }
 }
